69160e78cc74bc78c612b21e13e09a27218f0c4b
Gerardo Cruz <gerardhdez123@gmail.com>
2026-06-10 14:47:40 -0600

    Agregar archivos de proyecto.

---
+++ pryProyecto/frmLogin.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace pryProyecto
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            txtUsuario = new TextBox();
+            txtPassword = new TextBox();
+            pcbImagenLogin = new PictureBox();
+            btnAcceder = new Button();
+            btnSalir = new Button();
+            ((System.ComponentModel.ISupportInitialize)pcbImagenLogin).BeginInit();
+            SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(306, 92);
+            txtUsuario.Margin = new Padding(3, 2, 3, 2);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.PlaceholderText = "Ingrese el Usuario";
+            txtUsuario.Size = new Size(245, 23);
+            txtUsuario.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(306, 148);
+            txtPassword.Margin = new Padding(3, 2, 3, 2);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PlaceholderText = "Ingrese la Contraseña";
+            txtPassword.Size = new Size(245, 23);
+            txtPassword.TabIndex = 1;
+            txtPassword.UseSystemPasswordChar = true;
+            // 
+            // pcbImagenLogin
+            // 
+            pcbImagenLogin.Image = (Image)resources.GetObject("pcbImagenLogin.Image");
+            pcbImagenLogin.Location = new Point(12, 63);
+            pcbImagenLogin.Margin = new Padding(3, 2, 3, 2);
+            pcbImagenLogin.Name = "pcbImagenLogin";
+            pcbImagenLogin.Size = new Size(233, 177);
+            pcbImagenLogin.SizeMode = PictureBoxSizeMode.StretchImage;
+            pcbImagenLogin.TabIndex = 2;
+            pcbImagenLogin.TabStop = false;
+            // 
+            // btnAcceder
+            // 
+            btnAcceder.Location = new Point(306, 208);
+            btnAcceder.Margin = new Padding(3, 2, 3, 2);
+            btnAcceder.Name = "btnAcceder";
+            btnAcceder.Size = new Size(89, 32);
+            btnAcceder.TabIndex = 3;
+            btnAcceder.Text = "Acceder";
+            btnAcceder.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            btnSalir.Location = new Point(453, 208);
+            btnSalir.Margin = new Padding(3, 2, 3, 2);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(89, 32);
+            btnSalir.TabIndex = 4;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(700, 338);
+            Controls.Add(btnSalir);
+            Controls.Add(btnAcceder);
+            Controls.Add(pcbImagenLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsuario);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "frmLogin";
+            Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)pcbImagenLogin).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtUsuario;
+        private TextBox txtPassword;
+        private PictureBox pcbImagenLogin;
+        private Button btnAcceder;
+        private Button btnSalir;
+    }
+}
